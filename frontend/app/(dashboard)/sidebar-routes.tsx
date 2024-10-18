198235ebb2f4e9f4457d6f8a4ccf87d9d49f9787
Bahiyu <tankpiyush989321@gmail.com>
2024-10-18 21:51:05 +0530

    home page

---
+++ frontend/app/(dashboard)/sidebar-routes.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { 
+  CreditCard, 
+  Crown, 
+  Home, 
+  MessageCircleQuestion
+} from "lucide-react";
+import { usePathname } from "next/navigation";
+
+ 
+
+import { Button } from "../components/ui/button";
+import { Separator } from "../components/ui/separator";
+
+import { SidebarItem } from "./sidebar-item";
+
+export const SidebarRoutes = () => {
+  
+
+  const pathname = usePathname();
+ 
+
+
+  
+
+  return (
+    <div className="flex flex-col gap-y-4 flex-1">
+    
+      <ul className="flex flex-col gap-y-1 px-3">
+        <SidebarItem
+          href="/"
+          icon={Home}
+          label="Home"
+          isActive={pathname === "/"}
+        />
+      </ul>
+      <div className="px-3">
+        <Separator />
+      </div>
+      <ul className="flex flex-col gap-y-1 px-3">
+        {/* <SidebarItem
+          href={pathname}
+          icon={CreditCard}
+          label="Billing"
+          onClick={onClick}
+        />
+        <SidebarItem
+          href="mailto:support@codewithantonio.com"
+          icon={MessageCircleQuestion}
+          label="Get Help"
+        /> */}
+      </ul>
+    </div>
+  );
+};
